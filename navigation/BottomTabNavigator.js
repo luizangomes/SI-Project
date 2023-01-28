e184eed79d0ab0d75ab4d6206c53e96b31a1f771
Luiza N Gomes <37775707+luizangomes@users.noreply.github.com>
2023-01-28 12:06:32 -0300

    Revert "Contém a barra de tarefas."

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,145 +1,107 @@
-/* Tutorial que eu segui pra fazer este código:
-https://www.youtube.com/watch?v=gPaBicMaib4
-Ele também ensina a colocar sombras, mas não usei isso.
+// Learn more about createBottomTabNavigator:
+// https://reactnavigation.org/docs/bottom-tab-navigator
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import { useColorScheme } from "react-native";
 
-Ver também o comentário de "Stanisław Jarocki"
-
-Falta colocar cor de foco (linha 45 em diante).
-*/
-
-
-
-import * as React from 'react';
-import { View, Text, Image } from 'react-native-web';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-// Telas
+import Colors from "../constants/Colors";
 import HomeScreen from "../screens/HomeScreen";
 import ProfilePageScreen from "../screens/ProfilePageScreen";
-import PlaceholderScreen from "../screens/PlaceholderScreen";
 import SymptomsFeedScreen from "../screens/SymptomsFeedScreen";
 
-const Tab = createBottomTabNavigator();
+const BottomTab = createBottomTabNavigator();
 
+export default function BottomTabNavigator() {
+  const colorScheme = useColorScheme();
 
-export default function BottomTabNavigator(){
   return (
-    <Tab.Navigator
-      screenOptions = {{
-        tabBarShowLabel: false,
-        tabBarStyle: {
-          position: 'absolute',
-          bottom: 25,
-          left: 20,
-          right: 20,
-          elevation: 0,
-          backgroundColor: '#0D1F1BBF',
-          borderRadius: 15,
-          height: 90,
-        }
-      }}
-
-      initialRouteName = 'HomeScreen'
-    > 
-      <Tab.Screen name = "Perfil" component = {ProfilePageScreen}
-      options = {{
-        tabBarIcon: () => (
-          <View style = {{alignItems: 'center', justifyContent: 'center', top: 0}}>
-            <Text
-            style ={{color: '#fff', fontSize: 15, }}>Perfil
-            </Text>
-            <Image
-            source = {require('../assets/icons/perfil.png')}
-            resizeMode = 'contain'
-            style = {{
-              width: 45,
-              height: 45,
-            }}
-            />
-          </View>
-        ),
-      }}
+    <BottomTab.Navigator
+      initialRouteName="TabOne"
+      screenOptions={{ tabBarActiveTintColor: Colors[colorScheme].tint }}
+    >
+      <BottomTab.Screen
+        name="HealthApp"
+        component={HomeNavigator}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="md-home-outline" color={color} />
+          ),
+        }}
       />
-
-      <Tab.Screen name = "Vacinas" component = {PlaceholderScreen}
-            options = {{
-              tabBarIcon: () => (
-                <View style = {{alignItems: 'center', justifyContent: 'center', top: 0}}>
-                  <Text
-                  style ={{color: '#fff', fontSize: 15, }}>Vacinas
-                  </Text>
-                  <Image
-                  source = {require('../assets/icons/vacina.png')}
-                  resizeMode = 'contain'
-                  style = {{
-                    width: 45,
-                    height: 45,
-                  }}
-                  />
-                </View>
-              ),
-            }}
+      <BottomTab.Screen
+        name="FeedSymp"
+        component={SymptomsFeedNavigator}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="ios-medkit-outline" color={color} />
+          ),
+        }}
       />
-
-      <Tab.Screen name = "Home" component = {HomeScreen}
-            options = {{
-              tabBarIcon: () => (
-                <View style = {{alignItems: 'center', justifyContent: 'center', top: 0}}>
-                  <Image
-                  source = {require('../assets/icons/home.png')}
-                  resizeMode = 'contain'
-                  style = {{
-                    width: 55,
-                    height: 55,
-                  }}
-                  />
-                </View>
-              ),
-            }}
+      <BottomTab.Screen
+        name="Profile"
+        component={ProfilePageNavigator}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="people-circle" color={color} />
+          ),
+        }}
       />
+    </BottomTab.Navigator>
 
-      <Tab.Screen name = "Remédios" component = {PlaceholderScreen}
-            options = {{
-              tabBarIcon: () => (
-                <View style = {{alignItems: 'center', justifyContent: 'center', top: 0}}>
-                  <Text
-                  style ={{color: '#fff', fontSize: 15, }}>Remédios
-                  </Text>
-                  <Image
-                  source = {require('../assets/icons/remedios.png')}
-                  resizeMode = 'contain'
-                  style = {{
-                    width: 45,
-                    height: 45,
-                  }}
-                  />
-                </View>
-              ),
-            }}
+  );
+}
+
+// You can explore the built-in icon families and icons on the web at:
+// https://icons.expo.fyi/
+function TabBarIcon(props) {
+  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+}
+
+// Each tab has its own navigation stack, you can read more about this pattern here:
+// https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
+
+const HomeStack = createStackNavigator();
+
+function HomeNavigator() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="HealthApp"
+        component={HomeScreen}
+        options={{ headerTitle: "Health App" }}
       />
+    </HomeStack.Navigator>
+  );
+}
 
-      <Tab.Screen name = "Sintomas" component = {SymptomsFeedScreen}
-            options = {{
-              tabBarIcon: () => (
-                <View style = {{alignItems: 'center', justifyContent: 'center', top: 0}}>
-                  <Text
-                  style ={{color: '#fff', fontSize: 15, }}>Sintomas
-                  </Text>
-                  <Image
-                  source = {require('../assets/icons/sintomas.png')}
-                  resizeMode = 'contain'
-                  style = {{
-                    width: 45,
-                    height: 45,
-                  }}
-                  />
-                </View>
-              ),
-            }}
+const ProfilePageStack = createStackNavigator();
+
+function ProfilePageNavigator() {
+  return (
+    <ProfilePageStack.Navigator>
+      <ProfilePageStack.Screen
+        name="ProfilePage"
+        component={ProfilePageScreen}
+        options={{ headerTitle: "Meu Perfil" }}
       />
+    </ProfilePageStack.Navigator>
+  );
+}
 
-    </Tab.Navigator>
+const SymptomsFeedStack = createStackNavigator();
+
+function SymptomsFeedNavigator() {
+  return (
+    <SymptomsFeedStack.Navigator>
+      <SymptomsFeedStack.Screen
+        name="SymptomsFeed"
+        component={SymptomsFeedScreen}
+        options={{ headerTitle: "Histórico de Sintomas" }}
+      />
+    </SymptomsFeedStack.Navigator>
   );
 }
